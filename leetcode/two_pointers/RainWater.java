ee320071c1b35c7e3111a49c1b4b6128df2a8b73
Arun R <arun@DreamOne-2.local>
2026-02-16 17:39:37 -0600

    add 42. Rain Water

---
+++ leetcode/two_pointers/RainWater.java
@@ -0,0 +1,43 @@
+/*==42. Trapping Rain Water==*/
+
+class Template {
+  /*-Input Vars-*/
+  int[] height;
+  int n = height.length;
+
+  /*-Intermediate Vars-*/
+  int left = 0;
+  int right = n - 1;
+  int leftMax = 0;
+  int rightMax = 0;
+
+  /*-Output Var-*/
+  int totalWater = 0;
+
+  public int trap(int[] height) {
+    while (left < right) {
+      int leftH = height[left];
+      int rightH = height[right];
+      int currentWater = 0;
+
+      if (leftH < rightH) {
+        if (leftMax > leftH) {
+          currentWater = leftMax - leftH;
+        } else {
+          leftMax = leftH;
+        }
+        left++;
+      } else {
+        if (rightMax > rightH) {
+          currentWater = rightMax - rightH;
+        } else {
+          rightMax = rightH;
+        }
+        right--;
+      }
+
+      totalWater += currentWater;
+    }
+  }
+
+}
